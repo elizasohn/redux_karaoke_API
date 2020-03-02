59ca7261e89929e54a39ca281560ce3d629e253c
Student <example@example.com>
2020-03-02 10:16:26 -0800

    add fetchSongId action

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,4 +1,19 @@
 import * as types from './../constants/ActionTypes';
+import v4 from 'uuid/v4';
+
+export function fetchSongId(title) {
+    return function (dispatch) {
+        const localSongId = v4();
+        dispatch(requestSong(title, localSongId));
+        title = title.replace(' ', '_');
+        return fetch('http://api.musixmatch.com/ws/1.1/track.search?&q_track=' + title + '&page_size=1&s_track_rating=desc&apikey=YOUR-API-KEY-HERE').then(
+            response => response.json(),
+            error => console.log('An error occurred.', error)
+        ).then(function(json) {
+            console.log('CHECK OUT THIS SWEET API RESPONSE:', json)
+        });
+    };
+}
 
 export const nextLyric = (currentSongId) => ({
     type: types.NEXT_LYRIC,
@@ -13,4 +28,10 @@ export const restartSong = (currentSongId) => ({
 export const changeSong = (newSelectedSongId) => ({
     type: types.CHANGE_SONG,
     newSelectedSongId
-})
+});
+
+export const requestSong = (title, localSongId) => ({
+    type: types.REQUEST_SONG,
+    title,
+    songId: localSongId
+});
